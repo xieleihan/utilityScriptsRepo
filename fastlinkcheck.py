817f08bc76af98522a0a2b778d15d5e5a98290f2
xieleihan <xieleihan@gmail.com>
2025-08-26 10:27:19 +0800

    fix:修复已知的问题

--- fastlinkcheck.py
+++ fastlinkcheck.py
@@ -1,3 +1,5 @@
+import json
+
 import requests
 import os
 import sys
@@ -8,8 +10,11 @@ load_dotenv()
 
 login_url = os.getenv("LOGIN_URL")
 checkout_url = os.getenv("CHECKOUT_URL")
+logout_url = os.getenv("LOGOUT_URL")
 useremail = os.getenv("USEREMAIL")
 password = os.getenv("PASSWORD")
+
+print("签到的域名", checkout_url)
 
 # 模拟真实用户
 headers = {
@@ -39,7 +44,19 @@ if response.status_code == 200:
         resp_checkin = session.post(checkout_url,
                                     headers={'User-Agent': headers['User-Agent'], 'Accept': headers['Accept']})
 
-        print("Checkin response:", resp_checkin.text)
+        # print("Checkin response:", resp_checkin.text)
+        parsed = json.loads(resp_checkin.text)
+        if parsed.get("code") == 1:
+            print("签到成功:", parsed.get("msg"))
+        else:
+            print("签到失败:", parsed.get("msg"))
+        print("运行退出登录")
+        try:
+            resp_logout = session.post(logout_url,
+                                       headers={'User-Agent': headers['User-Agent'], 'Accept': headers['Accept']})
+        except requests.exceptions.JSONDecodeError:
+            print("退出失败")
+
     except requests.exceptions.JSONDecodeError:
         sys.exit(1)
 else:
