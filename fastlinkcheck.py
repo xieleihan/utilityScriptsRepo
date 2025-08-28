0594199b1beab8e544178d278ed86fbd712518a2
xieleihan <xieleihan@gmail.com>
2025-08-28 10:37:15 +0800

    feat:扩展签到脚本

--- fastlinkcheck.py
+++ fastlinkcheck.py
@@ -7,11 +7,15 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+useremail = os.getenv("USEREMAIL")
 login_url = os.getenv("LOGIN_URL")
 checkout_url = os.getenv("CHECKOUT_URL")
 logout_url = os.getenv("LOGOUT_URL")
-useremail = os.getenv("USEREMAIL")
 password = os.getenv("PASSWORD")
+ikuuu_password = os.getenv("IKUUU_PASSWORD")
+ikuuu_login_url = os.getenv("IKUUU_LOGIN_URL")
+ikuuu_checkout_url = os.getenv("IKUUU_CHECKOUT_URL")
+ikuuu_logout_url = os.getenv("IKUUU_LOGOUT_URL")
 
 # 模拟真实用户
 headers = {
@@ -35,22 +39,23 @@ response = session.post(
     # verify=False  # 仅用于测试，生产环境建议使用真实证书
 )
 
-print("登录响应状态码:", response.status_code)
+print("Fastlink登录响应状态码:", response.status_code)
 if response.status_code == 200:
-    print("登录成功😊")
+    print("Fastlink登录成功😊")
     try:
         data = session.cookies.get_dict()
         resp_checkin = session.post(checkout_url,
                                     headers={'User-Agent': headers['User-Agent'], 'Accept': headers['Accept']})
         parsed = json.loads(resp_checkin.text)
         if parsed.get("ret") == 1:
-            print("✅ 签到成功:", parsed.get("msg"))
+            print("✅ Fastlink签到成功:", parsed.get("msg"))
         else:
-            print("❌ 签到失败:", parsed.get("msg"))
-        print("运行退出登录")
+            print("❌ Fastlink签到失败:", parsed.get("msg"))
+        print("Fastlink运行退出登录")
         try:
             resp_logout = session.post(logout_url,
                                        headers={'User-Agent': headers['User-Agent'], 'Accept': headers['Accept']})
+            print("Fastlink退出登录了")
         except Exception as e:
             print("退出登录请求失败:", str(e))
 
@@ -59,3 +64,43 @@ if response.status_code == 200:
         sys.exit(1)
 else:
     sys.exit(1)
+
+session_1 = requests.Session()
+login_data = {
+    "host": 'ikuuu.org',
+    "email": useremail ,
+    "passwd": ikuuu_password,
+    "code":''
+}
+
+response_1 = session_1.post(
+    ikuuu_login_url,
+    json=login_data,
+    headers=headers,
+    allow_redirects=True,
+    # verify=False  # 仅用于测试，生产环境建议使用真实证书
+)
+
+print("Ikuuu登录响应状态码:", response_1.status_code)
+if response_1.status_code == 200:
+    print("Ikuuu登录成功😊")
+    try:
+        data = session_1.cookies.get_dict()
+        resp_checkin = session_1.post(ikuuu_checkout_url,
+                                    headers={'User-Agent': headers['User-Agent'], 'Accept': headers['Accept']})
+        parsed = json.loads(resp_checkin.text)
+        if parsed.get("ret") == 1:
+            print("✅ Ikuuu签到成功:", parsed.get("msg"))
+        else:
+            print("❌ Ikuuu签到失败:", parsed.get("msg"))
+        print("Ikuuu运行退出登录")
+        try:
+            resp_logout = session_1.post(ikuuu_logout_url,
+                                       headers={'User-Agent': headers['User-Agent'], 'Accept': headers['Accept']})
+            print("Ikuuu退出登录了")
+        except Exception as e:
+            print("退出登录请求失败:", str(e))
+
+
+    except requests.exceptions.JSONDecodeError:
+        sys.exit(1)
